0b527e7d9958b0df7ac943c12e9224276c78c351
Albert Kirakosyan <albert69kirakosyan@gmail.com>
2023-09-17 12:16:51 +0400

    flask

--- main_site.py
+++ main_site.py
@@ -2,14 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "<h1>Main</h1>"
+
 
 @app.route("/about")
 def about():
     return "<h1>about site</h1>"
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
