4f0e55f5a439f4d3ed396e5442b879bd172d4d47
Albert Kirakosyan <albert69kirakosyan@gmail.com>
2023-09-21 20:45:38 +0400

    flask

--- main_site.py
+++ main_site.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html')
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html')
 
 
 @app.route("/contact")
 def contact():
+    print(url_for('contact'))
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
